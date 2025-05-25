e002e7073e74a9167e2f56e235be6050828ff7bb
xiangliu1103 <xiang.liu.1@stonybrook.edu>
2025-05-25 16:37:41 -0400

    final

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from urllib.request import urlopen as uReq  # import the library to open web url
 import urllib
 from bs4 import BeautifulSoup as soup 
 
+# parsing the newegg graphics card search site 
 my_url= "https://www.newegg.com/p/pl?d=graphics+card"   # url of webpage for scraping
 
 headers= {'User-Agent': 'Mozilla/5.0'}
@@ -30,3 +31,10 @@ page_soup= soup(page_html, "html.parser")
 
 body_tag= page_soup.find("body") # find the body content 
 inner_div= body_tag.find("div", id= "app")
+page_div= inner_div.find("div", class_= "page-content")
+containers= page_div.find_all("div", class_="item-cell")
+
+
+# find the price of the items in the current webpage
+item=containers[0]
+print(item.find("li", class_= "price-current"))
